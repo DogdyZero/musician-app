8be41adafe4c834ed70645f20715dd4056d70307
unknown <dhlg_29@hotmail.com>
2019-08-16 16:03:07 -0300

    solve-merge

---
+++ src/app/memory-cupom-data-base.spec.ts
@@ -0,0 +1,7 @@
+import { MemoryCupomDataBase } from './memory-cupom-data-base';
+
+describe('MemoryCupomDataBase', () => {
+  it('should create an instance', () => {
+    expect(new MemoryCupomDataBase()).toBeTruthy();
+  });
+});
---
+++ src/app/memory-cupom-data-base.ts
@@ -0,0 +1,7 @@
+import { Cupom } from './model/cupom';
+
+export const MemoryCupomDataBase: Cupom[] =[
+    {codigo:'MUSIC100',valor:100},
+    {codigo:'MUSIC80',valor:80},
+    {codigo:'MUSIC70',valor:70},
+]
