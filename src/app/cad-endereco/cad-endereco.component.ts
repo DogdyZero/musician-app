0f292a119392bd892c3b971a2cb3e0333e006aff
unknown <dhlg_29@hotmail.com>
2019-08-17 12:01:07 -0300

    v00.16

--- src/app/cad-endereco/cad-endereco.component.ts
+++ src/app/cad-endereco/cad-endereco.component.ts
@@ -3,6 +3,7 @@ import {SelectItem, MenuItem} from 'primeng/api';
 import { Usuario } from '../model/usuario';
 import { HttpClient } from '@angular/common/http';
 import { Endereco } from '../model/endereco';
+import { Pessoa } from '../model/pessoa';
 
 @Component({
   selector: 'app-cad-endereco',
@@ -10,16 +11,16 @@ import { Endereco } from '../model/endereco';
   styleUrls: ['./cad-endereco.component.css']
 })
 export class CadEnderecoComponent implements OnInit {
-  @Input() usuario :Usuario;
-  @Output() updateUsuario = new EventEmitter();
+  constructor(private http:HttpClient) {}  
+  
+  @Input() pessoa :Pessoa;
+  @Output() update = new EventEmitter();
 
   @Input() id:number;
   @Output() updateId = new EventEmitter();
 
-  items: MenuItem[];
   tiposEndereco: SelectItem[];
   tipo: string;
-  cadastro =false;
 
   endereco:Endereco =new Endereco();
   enderecos:Endereco[]=[];
@@ -37,10 +38,7 @@ export class CadEnderecoComponent implements OnInit {
     this.http.get(url).subscribe(data=>{
       this.enderecoCorreios=data;}) 
   }
-  constructor(private http:HttpClient) {
-    
-  }
-
+  
   salvar(endereco:Endereco){   
     if(endereco.bairro==null){
       endereco.bairro = this.enderecoCorreios.bairro;
@@ -48,10 +46,9 @@ export class CadEnderecoComponent implements OnInit {
       endereco.estado = this.enderecoCorreios.uf;
     }
     endereco.tipoLogradouro=this.tipo;
-    this.cadastro=true;
     this.enderecos.push(endereco);
-    //this.usuario.endereco = this.enderecos;
-    this.updateUsuario.emit(this.usuario);
+    this.pessoa.endereco = this.enderecos;
+    this.update.emit(this.pessoa);
     this.updateId.emit(++this.id);
   }
 
@@ -60,12 +57,6 @@ export class CadEnderecoComponent implements OnInit {
       {label: 'Correspondência', value: 'Correspondência'},
       {label: 'Cobrança', value: 'Cobrança'},
       {label: 'Outros', value: 'Outros'}
-  ];
-    this.items = [
-      {label: 'Dados Pessoais'},
-      {label: 'Endereços'},
-      {label: 'Cartões'},
-      {label: 'Senha'}
     ];
   }
 
--- src/app/cad-senha/cad-senha.component.ts
+++ src/app/cad-senha/cad-senha.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { Usuario } from '../model/usuario';
-import { MenuItem } from 'primeng/api';
+import { Pessoa } from '../model/pessoa';
+import { Perfil } from '../model/perfil';
 
 @Component({
   selector: 'app-cad-senha',
@@ -8,26 +9,31 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./cad-senha.component.css']
 })
 export class CadSenhaComponent implements OnInit {
-  @Input() usuario :Usuario;
-  @Output() updateUsuario = new EventEmitter();
+  constructor() { }
+  
+  @Input() pessoa :Pessoa;
+  @Output() update = new EventEmitter();
 
   @Input() id:number;
   @Output() updateId = new EventEmitter();
 
-  salvar(usuario:Usuario){
-    this.updateUsuario.emit(this.usuario);
+  usuario:Usuario = new Usuario();
+  perfil:Perfil = new Perfil();
+  msg:string;
+  salvar(senha:string, senhaRepetida:string){
+    if(senha==senhaRepetida){
+      this.usuario.senha = senha;
+      this.perfil.nome ='cliente';
+      this.usuario.perfil = this.perfil;
+      this.usuario.login = this.pessoa.cpf;
+      this.pessoa.usuario = this.usuario;
+      this.update.emit(this.pessoa);
+    } else {
+      this.msg = 'As senha digitadas não conferem';
+    }
   }
-  items: MenuItem[];
-
-  constructor() { }
 
   ngOnInit() {
-    this.items = [
-      {label: 'Dados Pessoais'},
-      {label: 'Endereços'},
-      {label: 'Cartões'},
-      {label: 'Senha'}
-    ];
   }
 
 }
