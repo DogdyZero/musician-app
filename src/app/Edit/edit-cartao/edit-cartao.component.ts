72484244b6909bc9f246141577eac1fa124877e9
dogdyzero <dhlg_29@hotmail.com>
2019-10-02 00:42:20 -0300

    novos cadastros

--- src/app/Edit/edit-cartao/edit-cartao.component.ts
+++ src/app/Edit/edit-cartao/edit-cartao.component.ts
@@ -4,6 +4,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import {SelectItem, MenuItem} from 'primeng/api';
 import { Pessoa } from '../../model/pessoa';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { PessoasService } from 'src/app/services/pessoas.service';
 
 @Component({
   selector: 'app-edit-cartao',
@@ -12,9 +15,26 @@ import { Router } from '@angular/router';
 })
 export class EditCartaoComponent implements OnInit {
   @Input() pessoa:Pessoa
+  usuario:Usuario;
+  id=0;
+  totalCadastro =1; 
+  novoCadastro:boolean=false;
 
+  updateId(event){
+    this.id=event;
+    this.novoCadastro=false;
+  }
+  updateUsuario(usuario:Usuario){
+    let pessoa = this.usuario.pessoa;
+    this.pessoasService.alterarPessoa(pessoa).subscribe();
+  }
+  cadastrarNovo(){
+    this.novoCadastro=true;
+  }
 
-  constructor(private cartaoService:CartoesService,private router:Router) { }
+  constructor(private pessoasService:PessoasService,
+    private usuariosService:UsuariosService,
+    private cartaoService:CartoesService,private router:Router) { }
 
   async editCartao(cartao:Cartao){
     //criar uma copia do objeto
@@ -22,8 +42,8 @@ export class EditCartaoComponent implements OnInit {
     //let copyPessoa:Pessoa = new Pessoa();
     console.log(cartao)
 
-      this.cartaoService.alterarCartao(cartao).subscribe((data)=>{
-        this.router.navigate(['/usuario/',this.pessoa.id]);
+    this.cartaoService.alterarCartao(cartao).subscribe((data)=>{
+      this.router.navigate(['/usuario/',this.pessoa.id]);
         console.log(data);
       }, (error) => {
         console.log(error);
@@ -33,6 +53,8 @@ export class EditCartaoComponent implements OnInit {
   tipoBandeira: SelectItem[];
   selectedType: string;
   ngOnInit() {
+    this.usuario = this.usuariosService.getUsuario();
+
     this.tipoBandeira = [
       {label: 'Elo', value: 'Elo'},
       {label: 'Visa', value: 'Visa'},
--- src/app/Edit/edit-telefone/edit-telefone.component.ts
+++ src/app/Edit/edit-telefone/edit-telefone.component.ts
@@ -3,6 +3,9 @@ import { Telefone } from '../../model/telefone';
 import { Component, OnInit, Input } from '@angular/core';
 import { Pessoa } from '../../model/pessoa';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { PessoasService } from 'src/app/services/pessoas.service';
 
 @Component({
   selector: 'app-edit-telefone',
@@ -11,8 +14,26 @@ import { Router } from '@angular/router';
 })
 export class EditTelefoneComponent implements OnInit {
   @Input() pessoa:Pessoa
-  constructor(private telefonesService:TelefonesService,private router:Router) { }
+  usuario:Usuario;
+  id=0;
+  totalCadastro =1; 
+  novoCadastro:boolean=false;
 
+  updateId(event){
+    this.id=event;
+    this.novoCadastro=false;
+  }
+  updateUsuario(usuario:Usuario){
+    let pessoa = this.usuario.pessoa;
+    this.pessoasService.alterarPessoa(pessoa).subscribe();
+  }
+  constructor(private pessoasService:PessoasService,
+    private usuariosService:UsuariosService,
+    private telefonesService:TelefonesService,private router:Router) { }
+
+  cadastrarNovo(){
+    this.novoCadastro=true;
+  }
   alterarTelefone(telefone:Telefone){
     console.log(telefone)
     this.telefonesService.alterarTelefone(telefone).subscribe((data)=>{
@@ -24,6 +45,7 @@ export class EditTelefoneComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.usuario = this.usuariosService.getUsuario();
   }
 
 }
