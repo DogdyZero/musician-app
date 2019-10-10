a641b65b5bbe75c7d555d5a8e83460c7575a9737
dogdyzero <dhlg_29@hotmail.com>
2019-10-10 02:36:34 -0300

    estoque

--- src/app/Edit/edit-estoque/edit-estoque.component.ts
+++ src/app/Edit/edit-estoque/edit-estoque.component.ts
@@ -1,26 +1,34 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { ProdutosService } from './../../services/produtos.service';
+import { Subscription } from 'rxjs';
+import { Estoque } from 'src/app/model/estoque';
 import { Produto } from './../../model/produto';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ProdutosService } from './../../services/produtos.service';
 
 @Component({
   selector: 'app-edit-estoque',
   templateUrl: './edit-estoque.component.html',
   styleUrls: ['./edit-estoque.component.css']
 })
-export class EditEstoqueComponent implements OnInit {
+export class EditEstoqueComponent implements OnInit, OnDestroy {
   valorEstoque: string;
 
   @Input() produto: Produto;
   @Output() editProd = new EventEmitter()
   @Output() cancel = new EventEmitter()
 
+  estoque:Estoque;
+  inscricao:Subscription;
   constructor(
     private produtosService: ProdutosService,
     private router:Router
   ) { }
 
   ngOnInit() {
+    this.inscricao = this.produtosService.getProdutoEstoque(this.produto.id).subscribe(data=>{
+      this.estoque = data;
+      console.log(data);
+    })
   }
 
   editProduto(){
@@ -28,6 +36,9 @@ export class EditEstoqueComponent implements OnInit {
   }
   async cancelCad(){
     this.cancel.emit(null);
+  }
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
